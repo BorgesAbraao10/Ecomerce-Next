c1eb1d53b8b3692e424323730e930752ee2bed22
Abraão Borges <abraao10clear@gmail.com>
2024-03-27 14:14:39 -0300

    Last update

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -1,4 +1,4 @@
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  return <h1>Hello World!</h1>;
+  return <h1>Seja bem vindo(a) a nossa Loja!</h1>;
 }
