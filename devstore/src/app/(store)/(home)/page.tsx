ad7aa73558bd96f6df744d803a64dc739b43832f
Abraão Borges <abraao10clear@gmail.com>
2024-03-28 20:09:44 -0300

    Last update

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -1,4 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  return <h1>Seja bem vindo(a) a nossa Loja!</h1>;
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-ai-side.png"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className=" group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-java.png"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className=" group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-ia-p-devs.png"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+    </div>
+  );
 }
