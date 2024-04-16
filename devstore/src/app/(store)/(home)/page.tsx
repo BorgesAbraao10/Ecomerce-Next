7cdb471cd36173542f31d31bef90169bfd0a6dab
Abraão Borges <abraao10clear@gmail.com>
2024-04-16 10:41:36 -0300

    Last update

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -1,7 +1,20 @@
+import { api } from '@/data/api'
 import Image from "next/image";
 import Link from "next/link";
 
+async function getFeaturedProducts() {
+  const response = await api('/products/featured')
+
+  const products = await response.json()
+
+  return products
+}
+
 export default async function Home() {
+  const products = await getFeaturedProducts()
+
+  console.log(products)
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
@@ -30,11 +43,12 @@ export default async function Home() {
         className=" group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
-          className="group-hover:scale-105 transition-transform duration-500"
           src="/moletom-java.png"
+          className="group-hover:scale-105 transition-transform duration-500"
           width={920}
           height={920}
           quality={100}
+          alt=""
         />
 
         <div className="absolute bottom-5 rigth-5 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
@@ -55,6 +69,7 @@ export default async function Home() {
           width={920}
           height={920}
           quality={100}
+          alt=""
         />
 
         <div className="absolute bottom-5 rigth-5 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
